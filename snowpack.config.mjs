d245255dbf8c4cf74ba0ee57bce1c63d2e78c20d
Josh Mabry <jmabry@phase2technology>
2021-06-10 08:58:41 -0700

    add eslint

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -8,6 +8,14 @@ export default {
     '@snowpack/plugin-babel',
     '@snowpack/plugin-dotenv',
     [
+      '@canarise/snowpack-eslint-plugin',
+      {
+        globs: ['src/**/*.ts'], // You should provide this
+        options: {
+          /* any eslint options here */
+        },
+        formatter: 'stylish',
+      },
       '@snowpack/plugin-typescript',
       {
         /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
@@ -17,14 +25,14 @@ export default {
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    {"match": "routes", "src": ".*", "dest": "/index.html"},
+    { match: 'routes', src: '.*', dest: '/index.html' },
   ],
   optimize: {
-   bundle: true,
-   minify: true,
-   target: 'es2017',
-   treeshake: true,
-   splitting: true
+    bundle: true,
+    minify: true,
+    target: 'es2017',
+    treeshake: true,
+    splitting: true,
   },
   packageOptions: {
     /* ... */
