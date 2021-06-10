c6d39ad10649884d8abfca5df08d4f3336b1b8d0
Josh Mabry <jmabry@phase2technology>
2021-06-09 21:42:51 -0700

    add demo pages

---
+++ src/pages/page-about.ts
@@ -0,0 +1,28 @@
+import { html, css, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+
+@customElement('page-about')
+export class PageAbout extends LitElement {
+  static styles = css`
+    section {
+      padding: 1rem;
+    }
+  `;
+
+  render() {
+    return html`
+      <section>
+        <h1>About</h1>
+
+        <p>
+          Lorem ipsu m dolor sit amet, consectetur adipisicing elit. Eligendi,
+          delectus? Unde, sit. Fuga modi ea praesentium. Nemo dicta qui, magnam
+          cum dolorum excepturi beatae explicabo quidem fugiat ullam blanditiis
+          minima!
+        </p>
+      </section>
+    `;
+  }
+
+}
---
+++ src/pages/page-home.ts
@@ -0,0 +1,35 @@
+import { html, css, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('page-home')
+export class PageHome extends LitElement {
+  static styles = css`
+    section {
+      padding: 1rem;
+    }
+  `;
+
+  render() {
+    return html`
+      <section>
+        <h1>Home</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi,
+          delectus? Unde, sit. Fuga modi ea praesentium. Nemo dicta qui, magnam
+          cum dolorum excepturi beatae explicabo quidem fugiat ullam blanditiis
+          minima!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi,
+          delectus? Unde, sit. Fuga modi ea praesentium. Nemo dicta qui, magnam
+          cum dolorum excepturi beatae explicabo quidem fugiat ullam blanditiis
+          minima!
+        </p>
+
+        <p>Here you can see <a href="/error">the not found page</a>.</p>
+      </section>
+    `;
+  }
+}
