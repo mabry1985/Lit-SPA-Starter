68fe68b2c04c5e80390b426b184f10561388ed3c
Josh Mabry <jmabry@phase2technology>
2021-06-11 14:36:11 -0700

    add mock data for blog posts

---
+++ src/mock-data/blogs-data.ts
@@ -0,0 +1,17 @@
+export const blogs = [
+  {
+    author: 'Josh Mabry',
+    title: 'Hello World',
+    body: 'Sed in blandit leo. Maecenas ut convallis sapien, sed suscipit mi. Mauris convallis pellentesque ante. Fusce euismod nulla nec tellus congue, ac congue erat posuere. Integer sit amet mattis elit. Pellentesque sit amet orci eleifend, rutrum risus in, aliquet nulla. Nullam nec magna dignissim dolor eleifend pellentesque sed ac mauris. Morbi dictum dignissim enim nec tempus. Nulla justo risus, blandit sit amet aliquet vel, volutpat eget massa. Nam lobortis ante at nisl vestibulum ultricies. Mauris varius lorem eget nisi consectetur, sed pulvinar quam lacinia.',
+  },
+  {
+    author: 'Josh Mabry',
+    title: 'Hola Mundo',
+    body: 'Praesent rhoncus lacus enim, nec tristique nibh vulputate et. Nunc gravida, lorem vitae facilisis scelerisque, dui tellus vulputate sem, non accumsan ex urna non quam. Vestibulum et neque in nisi mattis porta. Etiam a aliquet mauris, sed egestas dui. Proin fermentum sapien sit amet eleifend commodo. Cras a elementum enim, scelerisque condimentum leo. Cras bibendum convallis dui, vel aliquam nisl tincidunt egestas. Ut ornare risus at consequat euismod. Suspendisse viverra non magna et ullamcorper. Quisque congue elit purus, et sollicitudin felis egestas consequat. Sed sit amet vestibulum elit, at consequat sapien. Phasellus vitae nisl pharetra, eleifend velit condimentum, tincidunt ipsum. Pellentesque ut leo ut nisl rhoncus sagittis eget in ipsum.',
+  },
+  {
+    author: 'Josh Mabry',
+    title: 'Hallo Welt',
+    body: 'Aliquam lacinia ultricies lectus, eu sollicitudin velit sodales at. Integer cursus dolor tempus tincidunt tincidunt. Aenean scelerisque ornare porttitor. Sed lacinia, leo viverra aliquet fringilla, lacus est malesuada lorem, in cursus sapien sapien vel leo. Pellentesque imperdiet sapien eu lectus vestibulum maximus. Cras eget leo scelerisque, lobortis sapien a, pellentesque nisi. Sed at egestas enim. Suspendisse dolor ante, interdum quis felis rhoncus, elementum elementum neque. Etiam ac egestas sapien. Donec quam est, accumsan id ipsum in, interdum convaldlis dolor. Quisque iaculis urna faucibus rutrum pulvinar. Nam bibendum ligula a eros congue lacinia. Suspendisse et facilisis dolor. Quisque iaculis porta enim, sit amet mollis turpis auctor nec.',
+  },
+];
