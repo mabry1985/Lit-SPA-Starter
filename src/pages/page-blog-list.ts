eba4a57501a6fe9153f56edc9ab068500b8649a4
Josh Mabry <jmabry@phase2technology>
2021-06-11 14:36:29 -0700

    add blog list and blog page

---
+++ src/pages/page-blog-list.ts
@@ -0,0 +1,50 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, state, property } from 'lit/decorators.js';
+import { blogs } from '../mock-data/blogs-data';
+import { router } from '../router';
+
+export interface Blog {
+  author: string;
+  title: string;
+  body: string;
+}
+
+@customElement('page-blog-list')
+class PageBlogList extends LitElement {
+  @state() blogPosts: Blog[] = [...blogs];
+
+  @property() location = router.location;
+
+  static styles = [
+    css`
+      :host {
+        display: block;
+      }
+
+      section {
+        padding: 1rem;
+      }
+    `,
+  ];
+
+  /* eslint-disable */
+  render() {
+    return html`
+      <section>
+        <h1>Blog Posts:</h1>
+        <ul class="blog-list">
+          ${this.blogPosts.map(
+            (blog, index) =>
+              html`
+                <li>
+                  <a href="blog/${index + 1}"> ${blog.title} </a>
+                </li>
+              `
+          )}
+        </ul>
+      </section>
+    `;
+  }
+}
+
+export default PageBlogList;
