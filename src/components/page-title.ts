49af3afd043c663dd815d74f1f2a36def25314b3
Josh Mabry <jmabry@phase2technology>
2021-06-12 14:15:24 -0700

    add page title component

---
+++ src/components/page-title.ts
@@ -0,0 +1,21 @@
+import { LitElement, html, css } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('page-title')
+export class PageTitle extends LitElement {
+  static styles = [
+    css`
+      :host {
+        display: block;
+      }
+      h1 {
+        font-size: 40px;
+        text-align: center;
+      }
+    `,
+  ];
+
+  render() {
+    return html` <h1><slot></slot></h1> `;
+  }
+}
