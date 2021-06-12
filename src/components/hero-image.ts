306855260f3491e4ae3aa120cd41e6db363094a3
Josh Mabry <jmabry@phase2technology>
2021-06-11 20:13:20 -0700

    build hero image component

---
+++ src/components/hero-image.ts
@@ -0,0 +1,29 @@
+import { LitElement, html, css } from 'lit';
+import { customElement } from 'lit/decorators.js';
+// import image from '../images/pdx-skyline.jpg';
+
+@customElement('hero-image')
+export class HeroImage extends LitElement {
+  static styles = [
+    css`
+      :host {
+        display: block;
+      }
+      .hero-image {
+        background-image: linear-gradient(
+            rgba(0, 0, 0, 0.1),
+            rgba(0, 0, 0, 0.3)
+          ),
+          url('/dist/images/sunset.jpg');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 50vh;
+        position: relative;
+        width: 100vw;
+      }
+    `,
+  ];
+
+  render = () => html` <div class="hero-image"></div> `;
+}
