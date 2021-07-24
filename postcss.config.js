06828c43e50fffe7b2139ce64e91c764aa70e4cc
Josh Mabry <jmabry@phase2technology>
2021-07-24 09:37:24 -0700

    add autoprefixer to plugins

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   syntax: require('@stylelint/postcss-css-in-js'),
   plugins: [
     require('tailwindcss')(),
+    require('autoprefixer'),
     require('postcss-discard-comments')(),
     require('postcss-discard-empty')(),
   ],
